14b00b9e1b1c1da9c9a692e84169f3f5c485f65a
THOLKAPPIYAN E <kappiyan22@gmail.com>
2025-06-12 12:01:26 +0530

    the addition program is done

--- c2ct_CoreJavaProgram/src/day1/Addition.java
+++ c2ct_CoreJavaProgram/src/day1/Addition.java
@@ -1,15 +1,11 @@
 package day1;
 
-public class SampleJava {
-
+public class Addition {
 	public static void main(String[] args) {
 		int a,b,c;
 		a=Integer.parseInt(args[0]);
 		b=Integer.parseInt(args[1]);
 		c=a+b;
 		System.out.print("addition of a="+a+" and b="+b+" is "+c);
-		// TODO Auto-generated method stub
-
+		}
 	}
-
-}
