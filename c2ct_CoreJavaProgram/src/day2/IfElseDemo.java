16f437ca3735824edd69531989007916472d34b3
THOLKAPPIYAN E <kappiyan22@gmail.com>
2025-06-14 20:58:54 +0530

    if,ifelseand nested loop

---
+++ c2ct_CoreJavaProgram/src/day2/IfElseDemo.java
@@ -0,0 +1,11 @@
+package day2;
+
+public class IfElseDemo {
+	public static void main(String[]argd) {
+		int age=11;
+		if(age<=17)
+			System.out.print("not eligible for voting");
+		else
+			System.out.print("elgible for the votting");
+	}
+}
